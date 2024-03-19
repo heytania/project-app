840c94c6a0e25bea315a4ac6955f033dcd5bfb2c
Tania <your@email.com>
2024-03-19 09:27:24 -0500

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require 'csv'
 require 'faker'
 
+# Define the path to the CSV file
 csv_file_path = Rails.root.join('news_sources.csv')
 
+# Define a method to parse date strings from the CSV
 def parse_date(date_str)
   formats = [
     '%d-%m-%Y %H:%M:%S',
@@ -19,17 +21,21 @@ def parse_date(date_str)
   nil
 end
 
-
+# Read data from the CSV file and populate the database
 CSV.foreach(csv_file_path, headers: true) do |row|
+  # Find or create Author
   author = Author.find_or_create_by!(name: row['AUTHOR'])
 
+  # Find or create Publisher
   publisher = Publisher.find_or_create_by!(name: row['PUBLISHER'])
 
+  # Find or create Country
   country = Country.find_or_create_by!(name: row['COUNTRY'])
 
+  # Find or create Category
   category = Category.find_or_create_by!(name: row['CATEGORY'])
   
-
+  # Create or update Article
   article = Article.find_or_create_by!(link: row['ARTICLE LINK']) do |article|
     article.published_date = parse_date(row['PUBLISHED DATE (UTC)'])
     if article.published_date.nil?
@@ -45,8 +51,37 @@ CSV.foreach(csv_file_path, headers: true) do |row|
     article.publisher = publisher
   end
 
+  # Create ArticleCountry association
   ArticleCountry.find_or_create_by!(article: article, country: country)
+  
+  # Create ArticleCategory association
   ArticleCategory.find_or_create_by!(article: article, category: category)
+end
+
+# Generate additional fake data using Faker gem
+50.times do
+  author = Author.find_or_create_by!(name: Faker::Name.name)
+  publisher = Publisher.find_or_create_by!(name: Faker::Company.name)
+  country = Country.find_or_create_by!(name: Faker::Address.country)
+  category = Category.find_or_create_by!(name: Faker::Book.genre)
+
+  article = Article.create!(
+    title: Faker::Lorem.sentence,
+    link: Faker::Internet.url,
+    published_date: Faker::Time.between(from: DateTime.now - 365, to: DateTime.now),
+    description: Faker::Lorem.paragraph,
+    image_url: Faker::LoremPixel.image,
+    video_url: Faker::Internet.url,
+    language: Faker::Nation.language,
+    author: author,
+    publisher: publisher
+  )
+
+  # Assign random categories to the article
+  article.categories << category
+
+  # Assign random countries to the article
+  article.countries << country
 end
 
 puts "Seeding completed!"
