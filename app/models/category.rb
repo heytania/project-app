31938245fa6b8659d38e425eff2d4dc0c091fdee
Tania <your@email.com>
2024-03-19 14:50:41 -0500

    final update

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
+# app/models/category.rb
 class Category < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  has_many :items
+end
+
+# app/models/item.rb
+class Item < ApplicationRecord
+  belongs_to :category
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module RailsIntroProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.google_maps_api_key = ENV['AIzaSyAsDpQLJJSWJmXrNW19o-jzE8qwq1eOImQ']
   end
 end
